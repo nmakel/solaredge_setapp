3251d46b80bfab0d59851d5dff773281e5391ff0
niels <51261230+nmakel@users.noreply.github.com>
2019-06-02 17:35:44 +0200

    modified directory reference

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 rm solaredge_setapp/*_pb2.py
-protoc --proto_path=./messages --python_out=solaredge_setapp/ messages/*.proto
+protoc --proto_path=messages/ --python_out=solaredge_setapp/ messages/*.proto
