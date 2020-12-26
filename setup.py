9f5b7b1ae5f96e5ae474de74fe9a18f0996e33b6
niels <51261230+nmakel@users.noreply.github.com>
2020-12-26 12:50:37 +0100

    bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="solaredge_setapp",
-    version="0.1.4",
+    version="0.1.5",
     description="SolarEdge SetApp protocol buffers parser library",
     long_description=long_description,
     long_description_content_type="text/markdown",
